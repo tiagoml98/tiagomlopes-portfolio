4bef05e7234cbb26d9a1646d066c0437548a935f
Lopes, Tiago <tiagoml98@gmail.com>
2023-08-24 17:03:57 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import type { Component } from 'solid-js'
+import type {Component} from 'solid-js'
+import Home from './pages/Home'
 
 const App: Component = () => {
-  return (
-    <p class="text-4xl text-green-700 text-center py-20">
-      This is a <a href="https://www.solidjs.com/">Solid</a> + <a href="https://tailwindcss.com/">Tailwind</a> + <a href="https://www.typescriptlang.org/">Typescript</a>!
-    </p>
-  )
+    return (
+        <div>
+            <Home/>
+        </div>
+    )
 }
 
 export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            <p>Hey, I'm Tiago</p>
+        </div>
+    )
+}
+
+export default Home
